dccfc8e70a86f9cd86da710de0078989b68d78df
Elias <elias0789@gmail.com>
2015-08-24 17:04:14 +0100

    add more detailed venue fields to form.

--- wp-content/plugins/form_plugin/form_plugin.php
+++ wp-content/plugins/form_plugin/form_plugin.php
@@ -14,12 +14,17 @@ function rest_event_form() {
     <input id="ev-summary" type="text" placeholder="Event name or summary" name="ev-summary"><br>
     Start date & time: <input id="ev-start-datetime" class="ev-datetime" type="datetime" name="ev-start-datetime"><br>
     End date & time: <input id="ev-end-datetime" class="ev-datetime" type="datetime" name="ev-end-datetime"><br>
-    <input type="text" id="ev-venue" placeholder="Venue/Location" name="ev-venue"><br>
     <input type="text" id="ev-group" placeholder="Group" name="ev-group"><br>
     <input type="url" id="ev-url" placeholder="URL for event info" name="ev-url"><br>
     <input type="url" id="ev-ticket-url" placeholder="URL for tickets" name="ev-ticket-url"><br>
     <input type="email" id="ev-email" placeholder="Organiser e-mail" name="ev-email"><br>
     <input type="text" id="ev-desc" placeholder="Enter event description" name="ev-description">
+    <input type="text" id="ev-venue-name" placeholder="Venue name" name="ev-venue-name"><br>
+    <input type="text" id="ev-venue-address" placeholder="Venue address" name="ev-venue-address"><br>
+    <input type="text" id="ev-venue-city" placeholder="City/Town" name="ev-venue-city"><br>
+    <input type="text" id="ev-venue-code" placeholder="Postcode" name="ev-venue-code"><br>
+    <input type="text" id="ev-venue-area" placeholder="Venue area/region" name="ev-venue-area"><br>
+    Country: <select id="ev-venue-country" name="ev-venue-country"><option value="GB">United Kingdom</option></select><br>
     <input type="checkbox" id="ev-is-physical" name="ev-is-physical" value="physical">The event is physical<br>
     <input type="checkbox" id="ev-is-virtual" name="ev-is-virtual" value="virtual">The event is virtual<br>
     <input type="submit" name="ev-submission" value="Submit Event">
