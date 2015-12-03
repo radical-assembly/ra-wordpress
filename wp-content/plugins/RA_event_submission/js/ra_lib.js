b577c1bbc7da3fc8a4799e34efca71352c11c1e8
Elias <elias0789@gmail.com>
2015-12-03 23:54:16 +0000

    add optional success callable to argument list.

--- wp-content/plugins/RA_event_submission/js/ra_lib.js
+++ wp-content/plugins/RA_event_submission/js/ra_lib.js
@@ -1,20 +1,21 @@
-function sendAjax($, isWithAuth, rType, rURL, rData, rDataType) {
+function sendAjax($, isWithAuth, rType, rURL, rData, rDataType, callable) {
     var authhead = (isWithAuth) ?
         {headers: {'Authorization': 'Basic ' + btoa('ra' + ':' + '**b@by**')}} :
         {};
+    var successCallback = (callable) ? {success: callable} : {};
 
     return $.ajax($.extend({
         type: rType,
         url: rURL,
         data: rData,
         dataType: rDataType,
-    }, authhead));
+    }, authhead, successCallback));
 }
 
-function sendAjaxGetJSON($, isWithAuth, url, data) {
-    return sendAjax($, isWithAuth, 'GET', url, data, 'json');
+function sendAjaxGetJSON($, isWithAuth, url, data, callable) {
+    return sendAjax($, isWithAuth, 'GET', url, data, 'json', callable);
 }
 
-function sendAjaxPostJSON($, isWithAuth, url, data) {
-    return sendAjax($, isWithAuth, 'POST', url, data, 'json');
+function sendAjaxPostJSON($, isWithAuth, url, data, callable) {
+    return sendAjax($, isWithAuth, 'POST', url, data, 'json', callable);
 }
