87e5e54cd8046c8a004df5664e2c3ef6030e0679
Elias <elias0789@gmail.com>
2015-12-03 17:15:03 +0000

    Commit current working version: Events and Venues load and display simple alerts when clicked, filter not working.

--- wp-content/plugins/RA_event_display/js/events.js
+++ wp-content/plugins/RA_event_display/js/events.js
@@ -1,28 +1,133 @@
 var tagList = '';
 var groupList = '';
+var domain = 'http://oac.dev';
+
+var iconParams = {
+    iconUrl: '/wp-content/plugins/radicalassembly_plugin/img/mapMarkerEventsIcon.png',
+    shadowUrl: '/wp-content/plugins/radicalassembly_plugin/img/mapMarkerShadow.png',
+    iconSize:     [25, 41], // size of the icon
+    shadowSize:   [41, 41], // size of the shadow
+    iconAnchor:   [12, 41], // point of the icon which will correspond to marker's location
+    shadowAnchor: [12, 41],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+};
 
 jQuery(document).ready(function($) {
-    console.log("Ready!");
+
     $('#calendar').fullCalendar({
         firstDay: 1,
         allDayDefault: false,
         eventStartEditable: false,
         eventDurationEditable: false,
+        timeFormat: 'H:mm',
         header: {
             left: 'title',
             centre: '',
             right: 'today prev,next',
         },
-        //eventSources: [
-        //    {
-        //        url: 'https://oac.dev/api1/radicalassembly/list/1/events.fullcalendar',
-        //        type: 'GET',
-        //        data: {
-        //            tags: tagList,
-        //            groups: groupList,
-        //        },
-        //        headers: {'Authorization': 'Basic ' + btoa('ra' + ':' + '**b@by**')}
-        //    }
-        //]
+        eventSources: [
+            {
+                url: domain + '/api1/radicalassembly/list/1/events.fullcalendar',
+                type: 'GET',
+                data: {
+                    tags: tagList,
+                    groups: groupList,
+                },
+                //headers: {'Authorization': 'Basic ' + btoa('ra' + ':' + '**b@by**')}
+            }
+        ],
+        eventClick: eventClickCallback,
+        loading: function(isLoading, view) {
+            eventLoadingCallback($, isLoading, view);
+        },
     });
+
+    map = initialiseMap(L);
 });
+
+jQuery('#filter-form').on('submit', function(e) {
+    e.preventDefault();
+
+    var checkThenPush = function(element, container) {
+        var tmp = [];
+        if (element.prop('checked')) tmp.push(element.attr('value'));
+        return container += tmp.join(',');
+    };
+
+    tagList = checkThenPush(jQuery('input[name="March"]'), tagList);
+    tagList = checkThenPush(jQuery('input[name="Demo"]'), tagList);
+    tagList = checkThenPush(jQuery('input[name="Discussion"]'), tagList);
+    tagList = checkThenPush(jQuery('input[name="Debate"]'), tagList);
+    groupList = checkThenPush(jQuery('input[name="GroupOne"]'), groupList);
+    groupList = checkThenPush(jQuery('input[name="GroupTwo"]'), groupList);
+    console.log(tagList, groupList);
+    jQuery('#calendar').fullCalendar('refetchEvents');
+});
+
+
+function eventClickCallback(ev, jsEvent, view) {
+    jQuery.get(domain + '/api1/event/' + ev.id + '/info.json')
+    .then(function(result) {
+        result = result.data[0];
+        alert(
+            result.summaryDisplay + '\n' +
+            'from: ' + result.start.displaylocal + '\n' +
+            'to: ' + result.end.displaylocal + '\n' +
+            'at: ' + result.venue.title + ', ' + result.venue.address + '\n' +
+            ''
+        );
+    });
+}
+
+function eventLoadingCallback($, isLoading, view) {
+    if (!isLoading) {
+        addEventVenuesToMap($, $('#calendar').fullCalendar('clientEvents'));
+    }
+}
+
+function initialiseMap(L) {
+    mapObj = new L.Map('map');
+
+    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
+
+    mapObj.addLayer(osm);
+    mapObj.setView(new L.LatLng(51.5057666596, -0.11682549681),10);
+
+    return mapObj;
+}
+
+// Assumes global 'map' variable exists
+function addEventVenuesToMap($, evObj) {
+    var seen = {}, markers = [];
+    for (var ii in evObj) {
+        if (evObj[ii].hasOwnProperty('venueid') && !seen.hasOwnProperty(evObj[ii].venueid)) {
+            seen[evObj[ii].venueid] = null;
+            var marker = L.marker(
+                [evObj[ii].lat, evObj[ii].lng],
+                {icon: L.icon(iconParams)}
+            );
+            marker.slug = evObj[ii].venueid;
+            marker.on('click', onClickMarker);
+            marker.addTo(map);
+            markers.push(marker);
+        }
+    }
+    return markers;
+}
+
+var onClickMarker = function() {
+    jQuery.get('http://oac.dev/api1/venue/'+this.slug+'/events.json')
+    .then(function(result) {
+        result = result.data[0];
+        alert(
+            'First Event here: \n' +
+            result.summaryDisplay + '\n' +
+            'from: ' + result.start.displaylocal + '\n' +
+            'to: ' + result.end.displaylocal + '\n' +
+            'at: ' + result.venue.title + ', ' + result.venue.address + '\n' +
+            ''
+        );
+    });
+};
