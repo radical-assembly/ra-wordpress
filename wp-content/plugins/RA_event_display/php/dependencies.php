1672211bde3b4bd2dcabbf5e0845837781b3497d
Elias <elias0789@gmail.com>
2015-12-03 17:13:40 +0000

    re-jig to try to get calendar styles to load.

--- wp-content/plugins/RA_event_display/php/dependencies.php
+++ wp-content/plugins/RA_event_display/php/dependencies.php
@@ -5,14 +5,16 @@
 * Enqueue the libraries and styles required for the displaying of the events
 */
 function ra_event_display_dependencies() {
-    wp_enqueue_script('jquery-ui-core');
     wp_enqueue_script('leaflet_script', plugins_url('../libs/leaflet.js', __FILE__), array(), false, true);
     wp_enqueue_script('moment_script', plugins_url('../libs/moment.min.js', __FILE__), array(), false, true);
-    wp_enqueue_script('fullcalendar_script', plugins_url('../libs/fullcalendar/fullcalendar.min.js', __FILE__), array('moment_script'), false, true);
+
     wp_enqueue_style('fullcalendar_main_style', plugins_url('../libs/fullcalendar/fullcalendar.min.css', __FILE__), array(), false, true);
-    wp_enqueue_style('fullcalendar_print_style', plugins_url('../libs/fullcalendar/fullcalendar.print.css', __FILE__), array(), false, true);
+    wp_enqueue_style('fullcalendar_print_style', plugins_url('../libs/fullcalendar/fullcalendar.print.css', __FILE__), array('fullcalendar_main_style'), false, true);
+
+    wp_enqueue_script('fullcalendar_script', plugins_url('../libs/fullcalendar/fullcalendar.min.js', __FILE__),
+    array('jquery', 'jquery-ui-core', 'jquery-ui-position', 'jquery-ui-selectmenu', 'moment_script'), false, true);
 }
 
-//if (is_page('events')) {
+//if (is_page(21)) {
     add_action( 'wp_enqueue_scripts', 'ra_event_display_dependencies');
 //}
