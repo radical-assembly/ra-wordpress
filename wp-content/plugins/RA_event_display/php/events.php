d031d0369925a53250f83c27e16b6c06e6f60789
Elias <elias0789@gmail.com>
2015-12-05 15:54:08 +0000

    is_page only works after plugins have been loaded.

--- wp-content/plugins/RA_event_display/php/events.php
+++ wp-content/plugins/RA_event_display/php/events.php
@@ -4,7 +4,4 @@
 function ra_event_display_scripts() {
     wp_enqueue_script('radicalassembly_event_calendar_map_script', plugins_url('../js/events.js', __FILE__), array('fullcalendar_script'), false, true);
 }
-
-//if (is_page('events')) {
-    add_action('wp_enqueue_scripts', 'ra_event_display_scripts');
-//}
+add_action('wp_enqueue_scripts', 'ra_event_display_scripts');
