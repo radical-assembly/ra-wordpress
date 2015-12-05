fd92085602b21c6269d66cbdc4e44fa38d0e2d0b
Elias <elias0789@gmail.com>
2015-12-05 00:23:16 +0000

    Remove static calendar -- using FullCalendar instead

--- wp-content/themes/radical-assembly/events.php
+++ wp-content/themes/radical-assembly/events.php
@@ -32,178 +32,24 @@
                                   <div id="content">
 <div id="calendar-wrap">
             <header>
-                <h1><i class ="iconicstroke-arrow-left"></i>August 2014<i class ="iconicstroke-arrow-right"></i></h1>
+				<div id="filter-wrap">
+                	<h2>Filter</h2>
+					<form id="filter-form">
+					    <strong>Tags</strong> |
+					    <input type="checkbox" name="tags" value="March"> March |
+					    <input type="checkbox" name="tags" value="Demonstration"> Demo |
+					    <input type="checkbox" name="tags" value="Discussion"> Discussion |
+					    <input type="checkbox" name="tags" value="Debate"> Debate |
+					    <br>
+					    <strong>Groups</strong> |
+					    <input type="checkbox" name="groups" value="TestGroup1"> TestGroup1 |
+					    <input type="checkbox" name="groups" value="TestGroup2"> TestGroup2 <br>
+					    <input type="submit" value="Apply Filter">
+						<input type="submit" value="Get iCal feed">
+					</form>
+				</div>
             </header>
             <div id="calendar">
-                <ul class="weekdays">
-                    <li>Sunday</li>
-                    <li>Monday</li>
-                    <li>Tuesday</li>
-                    <li>Wednesday</li>
-                    <li>Thursday</li>
-                    <li>Friday</li>
-                    <li>Saturday</li>
-                </ul>
-
-                <!-- Days from previous month -->
-
-                <ul class="days">
-                    <li class="day other-month">
-                        <div class="date">27</div>
-                    </li>
-                    <li class="day other-month">
-                        <div class="date">28</div>
-                    </li>
-                    <li class="day other-month">
-                        <div class="date">29</div>
-                    </li>
-                    <li class="day other-month">
-                        <div class="date">30</div>
-                    </li>
-                    <li class="day other-month">
-                        <div class="date">31</div>
-                    </li>
-
-                    <!-- Days in current month -->
-
-                    <li class="day">
-                        <div class="date">1</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">2</div>
-                    </li>
-                </ul>
-
-                    <!-- Row #2 -->
-
-                <ul class="days">
-                    <li class="day">
-                        <div class="date">3</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">4</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">5</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">6</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">7</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">8</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">9</div>
-                    </li>
-                </ul>
-
-                    <!-- Row #3 -->
-
-                <ul class="days">
-                    <li class="day">
-                        <div class="date">10</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">11</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">12</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">13</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">14</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">15</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">16</div>
-                    </li>
-                </ul>
-
-                    <!-- Row #4 -->
-
-                <ul class="days">
-                    <li class="day">
-                        <div class="date">17</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">18</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">19</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">20</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">21</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">22</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">23</div>
-                    </li>
-                </ul>
-
-                        <!-- Row #5 -->
-
-                <ul class="days">
-                    <li class="day">
-                        <div class="date">24</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">25</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">26</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">27</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">28</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">29</div>
-                    </li>
-                    <li class="day">
-                        <div class="date">30</div>
-                    </li>
-                </ul>
-
-                <!-- Row #6 -->
-
-                <ul class="days">
-                    <li class="day">
-                        <div class="date">31</div>
-                    </li>
-                    <li class="day other-month">
-                        <div class="date">1</div> <!-- Next Month -->
-                    </li>
-                    <li class="day other-month">
-                        <div class="date">2</div>
-                    </li>
-                    <li class="day other-month">
-                        <div class="date">3</div>
-                    </li>
-                    <li class="day other-month">
-                        <div class="date">4</div>
-                    </li>
-                    <li class="day other-month">
-                        <div class="date">5</div>
-                    </li>
-                    <li class="day other-month">
-                        <div class="date">6</div>
-                    </li>
-                </ul>
-
             </div><!-- /. calendar -->
         </div><!-- /. wrap -->
 
