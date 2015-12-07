54fd2635e07f936e61658e337b43bd5d193bd655
Elias <elias0789@gmail.com>
2015-12-07 01:11:59 +0000

    Simplify form HTML by removing unnecessary IDs

--- wp-content/plugins/RA_event_submission/php/event_submission.php
+++ wp-content/plugins/RA_event_submission/php/event_submission.php
@@ -1,36 +1,34 @@
-    <?php
+<?php
 /*
-*
-*/
-
-
-
-/**
 *
 */
 function event_form() {
     $form = '
     <form id="event-form">
-        <input id="ev-summary" type="text" placeholder="Event name or summary" name="ev-summary" required><br>
+        <input type="text" placeholder="Event name or summary" name="ev-summary" required><br>
 
-        Start date & time: <input id="ev-start-datetime" class="ev-datetime" type="datetime" name="ev-start-datetime" required><br>
-        End date & time: <input id="ev-end-datetime" class="ev-datetime" type="datetime" name="ev-end-datetime" required><br>
+        Start date & time: <input class="ev-datetime" type="text" name="ev-start-datetime" required><br>
+        End date & time: <input class="ev-datetime" type="text" name="ev-end-datetime" required><br>
 
-        <input type="checkbox" id="ev-is-physical" name="ev-is-physical" value="physical">The event is physical<br>
-        <input type="checkbox" id="ev-is-virtual" name="ev-is-virtual" value="virtual">The event is virtual<br>
+        <input type="radio" name="ev-realm" value="physical" required> This event is physical<br>
+        <input type="radio" name="ev-realm" value="virtual"> This event is virtual<br>
+        <input type="radio" name="ev-realm" value="both"> This event is both<br>
 
-        <input type="url" class="web-info" id="ev-url" placeholder="URL for event info" name="ev-url"><br>
-        <input type="url" class="web-info" id="ev-ticket-url" placeholder="URL for tickets" name="ev-ticket-url"><br>
-        <input type="email" class="web-info" id="ev-email" placeholder="Organiser e-mail" name="ev-email"><br>
+        <input type="url" class="web-info" placeholder="URL for event info" name="ev-url"><br>
+        <input type="url" class="web-info" placeholder="URL for tickets" name="ev-ticket-url"><br>
+        <input type="email" class="web-info" placeholder="Organiser e-mail" name="ev-email"><br>
 
-        <input type="text" id="ev-desc" placeholder="Enter event description" name="ev-description" required>
+        <textarea placeholder="Enter event description" name="ev-description" required></textarea>
 
-        <input type="text" class="venue-info" id="ev-venue-name" placeholder="Venue name" name="ev-venue-name"><br>
-        <input type="text" class="venue-info" id="ev-venue-address" placeholder="Venue address" name="ev-venue-address"><br>
-        <input type="text" class="venue-info" id="ev-venue-city" placeholder="City/Town" name="ev-venue-city"><br>
-        <input type="text" class="venue-info" id="ev-venue-code" placeholder="Postcode" name="ev-venue-code"><br>
-        <input type="text" class="venue-info" id="ev-venue-area" placeholder="Venue area/region" name="ev-venue-area"><br>
-        <div class="venue-info">Country:</div> <select class="venue-info" id="ev-venue-country" name="ev-venue-country"><option value="GB">United Kingdom</option></select><br>
+        <input type="text" class="venue-info" placeholder="Venue name" name="ev-venue-name"><br>
+        <input type="text" class="venue-info" placeholder="Venue address" name="ev-venue-address"><br>
+        <input type="text" class="venue-info" placeholder="City/Town" name="ev-venue-city"><br>
+        <input type="text" class="venue-info" placeholder="Postcode" name="ev-venue-code"><br>
+        <input type="text" class="venue-info" placeholder="Venue area/region" name="ev-venue-area"><br>
+        <div class="venue-info">
+            Country:
+            <select name="ev-venue-country"><option value="GB">United Kingdom</option></select>
+        </div><br>
 
         <input type="submit" name="ev-submission" value="Submit Event">
     </form>
@@ -51,7 +49,8 @@ function radicalassembly_form_datetime_picker() {
 add_action('wp_enqueue_scripts', 'radicalassembly_form_datetime_picker');
 
 function event_submission_scripts() {
+    wp_enqueue_script('validate_lib', plugins_url('../libs/validate.min.js', __FILE__), array(), false, true);
     wp_enqueue_script('radicalassembly_lib', plugins_url('../js/ra_lib.js', __FILE__), array(), false, true);
-    wp_enqueue_script('radicalassembly_event_submission', plugins_url('../js/event_submission.js', __FILE__), array('jquery', 'radicalassembly_lib', 'jquery-ui-timepicker-addon'), false, true);
+    wp_enqueue_script('radicalassembly_event_submission', plugins_url('../js/event_submission.js', __FILE__), array('jquery', 'radicalassembly_lib', 'jquery-ui-datetimepicker', 'validate_lib'), false, true);
 }
 add_action('wp_enqueue_scripts', 'event_submission_scripts');
