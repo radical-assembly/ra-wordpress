c69581caafc969eedc3510b102de1a8894f2252f
Elias <elias0789@gmail.com>
2015-12-08 11:41:39 +0000

    Add object to handle arranging data and populating html elements in the popup.

--- wp-content/plugins/RadicalAssembly/js/ra.events.js
+++ wp-content/plugins/RadicalAssembly/js/ra.events.js
@@ -1,7 +1,10 @@
 var params = {tags: '', groups: ''};
 var domain = 'https://oac.radicalassembly.com';
+var pop;
 
 jQuery(document).ready(function() {
+
+    pop = new EventPopUp(jQuery('#event-popup-wrap'));
 
     jQuery('#calendar').fullCalendar({
             firstDay: 1,
@@ -57,16 +60,24 @@ jQuery(document).ready(function() {
 
 
 function eventClickCallback(ev, jsEvent, view) {
-    sendAjaxGetJSON(jQuery, true, domain + '/api1/event/' + ev.id + '/info.json')
-    .then(function(result) {
-        result = result.data[0];
-        alert(
-            result.summaryDisplay + '\n' +
-            'from: ' + result.start.displaylocal + '\n' +
-            'to: ' + result.end.displaylocal + '\n' +
-            'at: ' + result.venue.title + ', ' + result.venue.address + '\n' +
-            ''
-        );
+    var p = sendAjaxGetJSON(jQuery, true, domain+'/api1/event/'+ev.id+'/info.json');
+    jQuery('#event-popup-wrap').dialog({
+        draggable: false,
+        modal: true,
+        resizable: false,
+        show: true,
+        hide: true,
+        open: function() {
+            p.then(function(result) {
+                result = result.data[0];
+                pop.setTitle(result.summaryDisplay);
+                pop.setStart(result.start.displaylocal);
+                pop.setEnd(result.end.displaylocal);
+                pop.setVenue(result.venue.title);
+                pop.setDesc(result.venue.description);
+            });
+        },
+        close: function() {pop.flush();},
     });
 }
 
@@ -74,4 +85,58 @@ function eventLoadingCallback(isLoading, view) {
     if (!isLoading) {
         refreshVenues(jQuery('#calendar').fullCalendar('clientEvents'));
     }
+}
+
+function EventPopUp(jqEv) {
+    var head = jQuery('<div id="popup-head"></div>');
+    var body = jQuery('<div id="popup-body"></div>');
+    var foot = jQuery('<div id="popup-foot"></div>');
+    jqEv.append(head).append(body).append(foot);
+
+    var title = jQuery('<div id="event-title"></div>');
+    var group = jQuery('<div id="event-group"></div>');
+    head.append(title).append(group);
+
+    var start = jQuery('<div id="event-start"></div>');
+    var end = jQuery('<div id="event-end"></div>');
+    var venue = jQuery('<div id="event-venue"></div>');
+    var desc = jQuery('<div id="event-description"></div>');
+    body.append(start).append(end).append(venue).append(desc);
+
+    this.setTitle = function(t) {
+        title.empty().append(t);
+    };
+
+    this.setGroup = function(g) {
+        group.empty().append(g);
+    };
+
+    this.setStart = function(t) {
+        start.empty().append(t);
+    };
+
+    this.setEnd = function(t) {
+        end.empty().append(t);
+    };
+
+    this.setVenue = function(v) {
+        venue.empty().append(v);
+    };
+
+    this.setDesc = function(d) {
+        desc.empty().append(d);
+    };
+
+    this.flush = function() {
+        title.empty();
+        group.empty();
+        start.empty();
+        end.empty();
+        venue.empty();
+        desc.empty();
+    };
+
+    this.destroy = function() {
+        jqEv.empty();
+    };
 }
